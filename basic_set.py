87a3c0342ad93eceb84cb509462cc235a947e47b
LCW <l035831@naver.com>
2021-05-21 11:53:08 +0900

    Set

---
+++ basic_set.py
@@ -0,0 +1,36 @@
+numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} # 전체 집합
+evens = {0, 2, 4, 6, 8} # 짝수 집합
+odds = {1, 3, 5, 7, 9} # 홀수
+mthree = {0, 3, 6, 9} # 3의 배수
+
+def define_set():
+    """
+    SET 정의 연습
+        set()
+        {}
+    """
+    empty = set() # 빈 셋트
+    print(empty, type(empty))
+    empty = {} # 빈 사전
+    print(empty, type(empty))
+
+    # 특성상 순서가 없고 인덱스도 없고 슬라이싱도 안된다
+    # 길이(파라미터의 수), 포함 여부 (in and not in)은 사용 가능
+    print(numbers, "LENGTH : " , len(numbers))
+    print("포함 여부 : ", 2 in numbers, 2 in evens, 2 in odds)
+
+    # 캐스팅 : 다른 순차형으로 Set 만들기
+    s = "Python Programming" # 문자열 안 쪽에 몇 개의 알파벳이 사용되었는가?
+    chars = set(s.upper())
+    print(s, chars)
+
+    #  특성상 중복 허용 안함
+    # list 의 중복값 제거에 유용함
+    lst  = "Python Programming Java Programming".upper().split()
+    print(lst)
+    words = set(lst)
+    print(words, len(words))
+
+
+if __name__ == "__main__":
+    define_set()
