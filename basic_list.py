c96f8b5938969368163abe00e8fce98006558b88
LCW <l035831@naver.com>
2021-05-21 11:34:45 +0900

    스택 과 큐 와 정렬

--- basic_list.py
+++ basic_list.py
@@ -86,15 +86,93 @@ def list_oper():
     lst[:0] = ['prepended']
     print(lst)
 
-    # 다양한 기초 산술 함수 제공 ㅋ
+    # 다양한 기초 산술 함수 제공
     lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     print("SUM : ", sum(lst))
     print("MIN : ", min(lst))
     print("MAX : ", max(lst))
     print("AVG : ", sum(lst) / len(lst))
 
+def list_method():
+    """
+    리스트의 메소드들
+    """
+    lst = [10, 2, 22, 9, 8, 33, 4, 12]
+    print("원본 : ", lst)
+    copy = lst.copy() # 복제 메소드
+    # Reverse : 리스트의 반전
+    copy.reverse()
+    print("Reverse : ", copy)
 
+    copy = lst.copy()
+    print("원본 : ", copy)
+
+    # Sorting
+    # 메소드로서의 sort -> 내부 데이터를 변경
+    # 문법으로서의 sort -> 데이터를 변경시키지 않고 정렬된 새 리스트를 반환
+
+    result = sorted(copy) # copy를 정렬 -> 새 리스트로 변환
+    print("SORTED ASC : " , result)
+    result = sorted(copy, reverse = True) # 내림차순 정렬
+    print("SORTED DESC : " , result)
+    #   정렬 키 함수 정의
+    # 정렬 키 함수를 전달 -> 정렬 기준을 변경
+    print("원본 : ", copy)
+    result = sorted(copy, key=str) # 키 함수를 str로 변경
+    print("SORTED key=str :", result)
+
+# 정렬 기준의 사용자 정의
+# 리스트의 요소를 5로 나눈 나머지 역순으로 정렬
+def key_func(val): # 사용자 정의 키 함수
+    return val % 5
+    result = sorted(copy, key=key_func(), reverse=True)
+
+    print("SORTED key=custom DESC : ", result)
+
+    # sorted 함수 -> 원본은 변경시키지 않음
+    # sort 메소드 -> 원본 내부 데이터 변경
+    copy.sort(key_func(), reverse=True)
+
+def stack_ex():
+    """
+    리스트를 활용한 스택의 구현
+        append and Pop 으로
+    """
+    stack = []
+    # 입력
+    stack.append(10)
+    stack.append(20)
+    stack.append(30)
+    print("STACK :", stack)
+
+    # 인출
+    print("POP : ", stack.pop())
+    print("POP : ", stack.pop())
+    print("POP : ", stack.pop())
+    if len(stack): # 스택이 비어있지 않으면 Pop 해라
+        print("POP : ", stack.pop())
+    else:
+        print("스택이 비어있습니다")
+
+def queue_ex():
+    """
+    리스트를 활용한 Queue의 구현
+        Append And POP
+    """
+    queue = []
+    queue.append(10)
+    queue.append(20)
+    queue.append(30)
+    print("QUEUE :", queue)
+
+    # 인출
+    print(queue.pop(0)) # 0번 째 인덱스 인출
+    print(queue.pop(0))
+    print(queue.pop(0))
 
 if __name__ == "__main__":
     #define_list()
-    list_oper()
+    #list_oper()
+   #list_method()
+    #stack_ex()
+    queue_ex()
