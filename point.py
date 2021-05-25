a8cdec0e6d0957d1af33e3ffde5c854117a52129
LCW <l035831@naver.com>
2021-05-25 16:02:31 +0900

    연산자 오버로딩

--- point.py
+++ point.py
@@ -15,3 +15,29 @@ class Point:
 
     def getY(self):
         return self.y
+
+    # 생성자 : __init__
+    def __init__(self, x = 0, y = 0): # x, y를 생성자 매개변수
+        self.x, self.y = x, y
+        Point.instance_count += 1
+
+    # 소멸자 : __del__
+    def __del__(self):
+        Point.instance_count -= 1
+
+    def __str__(self):
+        return "Point x = {}, y = {}".format(self.x, self.y)
+
+    def __repr__(self):
+        return "Point({}, {})".format(self.x, self.y)
+
+    def __add__(self, other):
+        # Point + other
+        if isinstance(other, Point):  # + Point
+            self.x += other.x
+            self.y += other.y
+        elif isinstance(other, int) # + int
+            self.x += other
+            self.y += other
+
+        return self
