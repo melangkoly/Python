16c25c61334652a8ded19cbfd8b414b4c1049b4b
LCW <l035831@naver.com>
2021-05-25 13:49:16 +0900

    모듈

--- adv_function_args.py
+++ adv_function_args.py
@@ -95,3 +95,12 @@ def clean_strings(strings, *funcs):
 cleaned = clean_strings(dirty_strings, str.strip, str.title)
 print("Cleaned : ", cleaned)
 
+#__name__ 속성
+#   import 될 때 -> 자신의 이름
+#   직접 실행될 때 -> __main__
+print("__name__:", __name__)
+
+if __name__ == "__main__":
+    print("최상위 모듈로 실행되었다")
+else:
+    print("import 된 모듈이다")
